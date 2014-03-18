6ef81b2e6d1e3a7b8f22c9d2202dcbbb32171f94
2ndkauboy <github@kau-boys.de>
2014-03-18 10:24:25 +0100

    first commit

---
+++ syntaxhighlighter-sass.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: SyntaxHighlighter Evolved: SASS Brush
+Description: Adds support for the SASS language to the SyntaxHighlighter Evolved plugin.
+Author: Bernhard kau
+Version: 1.0
+Author URI: http://kau-boys.de/
+*/
+ 
+// SyntaxHighlighter Evolved doesn't do anything until early in the "init" hook, so best to wait until after that
+add_action( 'init', 'syntaxhighlighter_sass_regscript' );
+ 
+// Tell SyntaxHighlighter Evolved about this new language/brush
+add_filter( 'syntaxhighlighter_brushes', 'syntaxhighlighter_sass_addlang' );
+ 
+// Register the brush file with WordPress
+function syntaxhighlighter_sass_regscript() {
+    wp_register_script( 'syntaxhighlighter-brush-sass', plugins_url( 'shBrushSass.js', __FILE__ ), array('syntaxhighlighter-core'), '1.2.3' );
+}
+ 
+// Filter SyntaxHighlighter Evolved's language array
+function syntaxhighlighter_sass_addlang( $brushes ) {
+    $brushes['sass'] = 'sass';
+    $brushes['scss'] = 'sass';
+ 
+    return $brushes;
+}
